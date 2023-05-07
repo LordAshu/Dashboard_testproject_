a4aed755efd6e85aa3bb549490816b7f0b51117c
Ashutosh Sharma <ashu.jp2050@gmail.com>
2023-05-08 00:55:56 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,27 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Line, Employees, Calendar, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
-
 import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 
 const App = () => {
-    const { activeMenu } = useStateContext();
+    const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
+
+    
 
 
   return (
-    <div>
+    <div className={ currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
             <TooltipComponent content="Settings" postion="TOp">
               <button type="button" className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-              style={{ background: 'blue', borderRadius: '50%'}}>
+              onClick={() => setThemeSettings(true)}
+              style={{ background: currentColor, borderRadius: '50%'}}>
                <FiSettings /> 
               </button>
             </TooltipComponent>
@@ -36,7 +37,7 @@ const App = () => {
             </div>
           )}
           <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+            `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
                }>
                 <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                   <Navbar />
@@ -44,6 +45,10 @@ const App = () => {
           
           
           <div>
+
+            {themeSettings && <ThemeSettings /> }
+
+
             <Routes>
               { /* Dashboard */}
               <Route path="/" element={<Ecommerce />} />
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { BsCurrencyDollar } from 'react-icons/bs';
 import { GoPrimitiveDot } from 'react-icons/go';
 import { Stacked, Pie, Button, SparkLine } from '../components';
-import { earningData, SparklineAreaData, ecomPiechartDat } from '../data/dummy';
+import { earningData, SparklineAreaData, ecomPiechartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
+  const  { currentColor, currentMode } = useStateContext();
+
   return (
     <div className='mt-12'>
       <div className='flex flex-wrap lg-:flex-nowrap justify-center'>
-        <div className=' bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl
-                           w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repat bg-cover bg-center'>
+        <div className=' bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repat bg-cover bg-center'>
           <div className='flex justify-between items-center'>
             <div>
               <p className='font-bold text-gray-400'>Earnings</p>
@@ -20,7 +21,7 @@ const Ecommerce = () => {
           <div className='mt-6'>
             <Button 
             color='white'
-            bgColor='blue'
+            bgColor={currentColor}
             text="Download"
             borderRadius='10px'
             size='md'
@@ -98,19 +99,19 @@ const Ecommerce = () => {
 
               <div className='mt-5'>
                {/*  <SparkLine 
-                currentColor='blue'
+                currentColor= {currentColor}
                 id='line-sparkline'
                 type='Line'
                 height='80px'
                 width='250px'
                 data={SparklineAreaData}
-          color='blue'/> */} 
+                color={currentColor}/> */} 
               </div>
               
               <div className='mt-10'>
                 <Button 
                 color='white'
-                bgColor='blue'
+                bgColor= {currentColor}
                 text='Download Report'
                 borderRadius='10px'/>
               </div>
